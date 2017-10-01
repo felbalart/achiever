844ed35fcacf9e3fc438d16e70827fe1cdf9b9d9
felbalart <felbalart@gmail.com>
2017-09-30 21:07:14 -0300

    feat(period): updates policy

--- app/policies/period_policy.rb
+++ app/policies/period_policy.rb
@@ -1,10 +1,23 @@
 class PeriodPolicy < ApplicationPolicy
+
+  def index?
+    user.role.sysadmin?
+  end
+
   def update?
     user.role.sysadmin?
   end
 
   def edit?
     update?
+  end
+
+  def new?
+    create?
+  end
+
+  def create?
+    user.role.sysadmin?
   end
 
   def set_as_current?
